32371b115b51e3a47b0d2b6334ceb2b4d39ad2fa
Yosolaprog <fvrogunleye@gmail.com>
2026-01-09 23:26:39 +0100

    Add files via upload

---
+++ ASSIGNMENTT/Sorting.py
@@ -0,0 +1,82 @@
+import time
+
+data = [5, 8, 16, 10, 3, 1, 4]
+
+# -------- Bubble Sort --------
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            
+print(bubble_sort([5,8,16,10,3,1,4]))
+# -------- Selection Sort --------
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print(selection_sort([5,8,16,10,3,1,4]))
+
+# -------- Insertion Sort --------
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+print(insertion_sort([5,8,16,10,3,1,4]))
+
+# -------- Merge Sort --------
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+print(merge_sort([5,8,16,10,3,1,4]))
+
+# -------- Quick Sort --------
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    left = [x for x in arr[:-1] if x <= pivot]
+    right = [x for x in arr[:-1] if x > pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+print(quick_sort([5,8,16,10,3,1,4]))
+
+
+
